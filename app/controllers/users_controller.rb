05b433f419bbd30b31c965c55af66e520d1233d9
ChayaGreisman <chayamgreisman@gmail.com>
2020-08-06 22:08:48 -0400

    render users index

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,12 @@
 class UsersController < ApplicationController
 
+    def index
+        users = User.all
+        # render :json => users.to_json(:include => {:accounts => {:include=> :transactions}})
+        render json: users, include:[:accounts]
+    end
+    
+    
     def create
         user=User.create(user_params)
         render json: user
